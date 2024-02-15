6a0535e51a8b6a52c7777e2ffb841bbaf80983d3
Salma El jamali <sel-jama@student.1337.ma>
2024-02-15 16:20:22 +0100

    working on templates

--- M07/ex02/Array.hpp
+++ M07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-jama <sel-jama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 12:29:52 by sel-jama          #+#    #+#             */
-/*   Updated: 2024/02/13 13:37:44 by sel-jama         ###   ########.fr       */
+/*   Updated: 2024/02/15 15:51:19 by sel-jama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,44 @@
 template <class T>
 class Array {
     private:
-        T array[];
+        unsigned int size;
+        T *array;
     public:
-        Array(){ array = new T; }//empty array creation
-        Array(unsigned int n){ } //n elements array creation
-        
-        //deep copy
-        Array(const Array& original){ *this = original; }
-        const Array& operator=(const Array& original) { 
-            for(int i = 0; i < original.size(); i++){
-                this->array[i] = new array[i];
-            }
-            return *this;
-        }
+        Array();//empty array creation
+        Array(unsigned int n); //n elements array creation
 
-        const unsigned int &size() const{
-            return (sizeof(this->array) / sizeof(T));
-        }
+        //deep copy
+        Array(const Array& original);
+        const Array& operator=(const Array& original);
+
+        const unsigned int &size();
 };
+
+template <class T>
+Array<T>::Array():array(NULL), size(0){}
+
+template <class T>
+Array<T>::Array(unsigned int n): size(n){
+    this->array = new T[n];
+}
+
+template <class T>
+Array<T>::Array(const Array& original){
+    *this = original;
+}
+
+template <class T>
+const Array<T> &Array<T>::operator=(const Array<T> &original){
+    this->size = original.size;
+    this->array = new T[size];
+    for (int i =0; i < size; i++){
+        this->array[i] = original.array[i];
+    }
+}
+
+template <class T>
+const unsigned int &Array<T>::size(){
+    return this->size;
+}
 
 #endif
