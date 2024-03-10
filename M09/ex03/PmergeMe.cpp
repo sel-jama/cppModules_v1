73914c631ff9f5eb12c2a945d02e228d60bdb329
Salma El jamali <sel-jama@student.1337.ma>
2024-03-10 08:55:29 +0000

    done

--- M09/ex03/PmergeMe.cpp
+++ M09/ex03/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-jama <sel-jama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 03:03:48 by sel-jama          #+#    #+#             */
-/*   Updated: 2024/03/10 06:23:03 by sel-jama         ###   ########.fr       */
+/*   Updated: 2024/03/10 08:45:35 by sel-jama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,52 @@
 PmergeMe::PmergeMe(const std::vector<int> &vec, const std::list<int> &lst) : vec(vec), lst(lst){   
 }
 
+PmergeMe::PmergeMe(const PmergeMe& other){
+    *this = other;
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe& other){
+    this->vec = other.vec;
+    this->lst = other.lst;
+    return *this;
+}
+
+PmergeMe::~PmergeMe(){}
+
+//starter function to sort and mesure two diffrent containers
+void PmergeMe::sortAndmesure(void){
+    std::vector<int> container1 = vec;
+    std::list<int> container2 = lst;
+
+    clock_t start1 = clock();
+    mergeInserSort(container1);
+    clock_t end1 = clock();
+    double time1 = static_cast<double>(end1 - start1) / CLOCKS_PER_SEC * 1000000;
+
+    std::cout << "Before: ";
+    printContainer(vec);
+
+    std::cout << "After : ";
+    printContainer(container1);
+
+    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << time1 << " us" << std::endl;
+    
+    std::cout << std::endl;
+    clock_t start2 = clock();
+    mergeInserSort(container2);
+    clock_t end2 = clock();
+    double time2 = static_cast<double>(end2 - start2) / CLOCKS_PER_SEC * 1000000;
+
+    std::cout << "Before: ";
+    printContainer(lst);
+
+    std::cout << "After : ";
+    printContainer(container2);
+
+    std::cout << "Time to process a range of " << lst.size() << " elements with std::list : " << time2 << " us" << std::endl;
+}
+
+//helper template functions
 template <typename T>
 void printContainer(T &container){
     typename T::iterator it = container.begin();
@@ -24,49 +70,21 @@ void printContainer(T &container){
     std::cout << std::endl;
 }
 
-void PmergeMe::sortAndmesure(void){
-    std::vector<int> container1 = vec;
-    std::list<int> container2 = lst;
-
-    clock_t start1 = clock();
-    mergeInserSort(container1);
-    clock_t end1 = clock();
-    double time1 = (double)(end1 - start1) / CLOCKS_PER_SEC * 1000000;
-
-    std::cout << "Before: ";
-    printContainer(vec);
-
-    std::cout << "After: ";
-    printContainer(container1);
-
-    std::cout << "Time to process a range of " << vec.size() << " elements with container 1: " << time1 << " us" << std::endl;
-    
-    std::cout << std::endl;
-    clock_t start2 = clock();
-    mergeInserSort(container2);
-    clock_t end2 = clock();
-    double time2 = (double)(end2 - start2) / CLOCKS_PER_SEC * 1000000;
-
-    std::cout << "Before: ";
-    printContainer(lst);
-
-    std::cout << "After: ";
-    printContainer(container2);
-
-    std::cout << "Time to process a range of " << lst.size() << " elements with container 1: " << time2 << " us" << std::endl;
-}
-
-bool isOddLength(std::vector<int>& arr) {
+template <typename T>
+bool isOddLength(T& arr) {
     return arr.size() % 2 != 0;
 }
 
-int removeStraggler(std::vector<int>& arr) {
+template <typename T>
+int removeStraggler(T& arr) {
     
     int straggler = arr.back();
     arr.pop_back();
     return straggler;
 }
 
+
+//insertion_sort for std::vector
 void insertion_sort_pairs(std::vector<std::pair<int, int> >& A, int n) {
     if (n <= 0) return;
     
@@ -123,85 +141,74 @@ void PmergeMe::mergeInserSort(std::vector<int> &v){
 
 
 
+//Insertion_sort for std::list
+std::list<std::pair<int, int> > createPairs(std::list<int>& lst) {
+    std::list<std::pair<int, int> > pairs;
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end();) {
+        int a = *it;
+        it++;
+        int b = *it;
+        it++;
+        pairs.push_back(std::make_pair(std::min(a, b), std::max(a, b)));
+    }
+    return pairs;
+}
 
-// bool isOddLength(std::list<int>& arr) {
-//     return arr.size() % 2 != 0;
-// }
-
-// int removeStraggler(std::list<int>& arr) {
-//     int straggler = arr.back();
-//     arr.pop_back();
-//     return straggler;
-// }
-
-// std::list<std::pair<int, int> > createPairs(std::list<int>& lst) {
-//     std::list<std::pair<int, int> > pairs;
-//     for (std::list<int>::iterator it = lst.begin(); it != lst.end();) {
-//         int a = *it;
-//         it++;
-//         int b = *it;
-//         it++;
-//         pairs.push_back(std::make_pair(std::min(a, b), std::max(a, b)));
-//     }
-//     return pairs;
-// }
-
-// void insertion_sort_pairs(std::list<std::pair<int, int> >& A, int n) {
-//     if (n <= 0) return;
+void insertion_sort_pairs(std::list<std::pair<int, int> >& A, int n) {
+    if (n <= 0) return;
     
-//     insertion_sort_pairs(A, n - 1);
+    insertion_sort_pairs(A, n - 1);
     
-//     std::list<std::pair<int, int> >::iterator it = A.begin();
-//     std::advance(it, n);
-//     std::pair<int, int> element = *it;
+    std::list<std::pair<int, int> >::iterator it = A.begin();
+    std::advance(it, n);
+    std::pair<int, int> element = *it;
     
-//     std::list<std::pair<int, int> >::iterator j = it;
-//     --j;
+    std::list<std::pair<int, int> >::iterator j = it;
+    --j;
     
-//     while (j != A.end() && element.second < j->second) {
-//         std::swap(*std::next(j), *j);
-//         --j;
-//     }
-// }
+    while (j != A.begin() && element.second < j->second) {
+        std::swap(*std::next(j), *j);
+        --j;
+    }
+    if (element.second < j->second)
+         std::swap(*std::next(j), *j);
+}
 
-// std::list<int> create_s(std::list<int>& pairs, int straggler) {
-//     std::list<int> S, pend;
+std::list<int> create_s(std::list<std::pair<int, int> >& pairs, int straggler) {
+    std::list<int> S, pend;
+    std::list<std::pair<int, int> >::iterator it = pairs.begin();
 
-//     S.push_back(pairs.front());
-//     pairs.pop_front();
-//     S.push_back(pairs.front());
-//     pairs.pop_front();
+    S.push_back(pairs.front().first);
+    S.push_back(pairs.front().second);
+    it++;
 
-//     while (!pairs.empty()) {
-//         S.push_back(pairs.front());
-//         pairs.pop_front();
-//         pend.push_back(pairs.front());
-//         pairs.pop_front();
-//     }
+    while (it != pairs.end()) {
+        S.push_back((*it).second);
+        pend.push_back((*it).first);
+        ++it;
+    }
 
-//     if (straggler != -1)
-//         pend.push_back(straggler);
+    if (straggler != -1)
+        pend.push_back(straggler);
 
-//     // pend.sort();
-//     std::list<int>::iterator it = pend.begin();
-//     while (it != pend.end()) {
-//         std::list<int>::iterator ins_it = std::lower_bound(S.begin(), S.end(), *it);
-//         S.insert(ins_it, *it);
-//         ++it;
-//     }
+    std::list<int>::iterator i = pend.begin();
+    while (i != pend.end()) {
+        std::list<int>::iterator ins_it = std::lower_bound(S.begin(), S.end(), *i);
+        S.insert(ins_it, *i);
+        ++i;
+    }
+    return S;
+}
 
-//     return S;
-// }
+void PmergeMe::mergeInserSort(std::list<int>& l){
+    if (l.size() <= 1)
+        return;
 
-// void PmergeMe::mergeInserSort(std::list<int>& l){
-//     if (l.size() <= 1)
-//         return;
-
-//     int straggler = -1;
-//     if (isOddLength(l))
-//         straggler = removeStraggler(l);
+    int straggler = -1;
+    if (isOddLength(l))
+        straggler = removeStraggler(l);
     
-//     std::list<std::pair<int, int> > pairs = createPairs(l);
-//     insertion_sort_pairs(pairs, pairs.size() - 1);
-//     l = create_s(l, straggler);
-// }
+    std::list<std::pair<int, int> > pairs = createPairs(l);
+    insertion_sort_pairs(pairs, pairs.size() - 1);
+    l = create_s(pairs, straggler);
+}
--- M09/ex03/PmergeMe.hpp
+++ M09/ex03/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-jama <sel-jama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 03:03:52 by sel-jama          #+#    #+#             */
-/*   Updated: 2024/03/10 05:54:12 by sel-jama         ###   ########.fr       */
+/*   Updated: 2024/03/10 08:35:10 by sel-jama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,15 @@
 
 class PmergeMe{
     private:
+        PmergeMe();
         std::vector<int> vec;
         std::list<int> lst;
     
     public:
         PmergeMe(const std::vector<int> &vec, const std::list<int> &lst);
+        PmergeMe(const PmergeMe& other);
+        PmergeMe &operator=(const PmergeMe& other);
+        ~PmergeMe();
     
         void sortAndmesure(void);
         void mergeInserSort(std::vector<int> &v);
